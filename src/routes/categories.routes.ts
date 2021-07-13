ded94e79e934f2bcf27afd18ba3f21efaadb2794
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-13 13:40:21 -0300

    SRP - Princípio de Responsabilidade Única

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import {request, response, Router} from 'express';
 import {CategoriesRepository} from "../repositories/CategoriesRepository";
+import {CreateCategoryService} from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -11,11 +12,12 @@ categoriesRoutes.get("/", (request, response) => {
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-    if(categoryAlreadyExists){
-        return response.status(400).json({error: "Category already exists!"});
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    try {
+        createCategoryService.execute({name, description});
+    }catch (e) {
+        return response.status(400).json({error: "Category already exists!"})
     }
-    categoriesRepository.create({name, description});
     return response.status(201).send();
 });
 
