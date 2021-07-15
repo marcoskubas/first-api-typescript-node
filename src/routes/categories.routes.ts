bb46856ef44c32cab2f51ab87aa2ac1924ef9813
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-14 21:05:32 -0300

    Aplicando Singleton Pattern

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import {request, response, Router} from 'express';
+import {Router} from 'express';
 import {createCategoryController} from "../modules/cars/useCases/createCategory/index";
 import {listCategoryController} from "../modules/cars/useCases/listCategories/index";
 
