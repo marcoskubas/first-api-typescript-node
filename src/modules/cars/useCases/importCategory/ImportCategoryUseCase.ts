16e3ce76e02de79e4fe8cc81c26f7c8ab88f96d7
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-15 20:39:03 -0300

    Implementação Inicial Stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,17 @@
+import fs from "fs";
+import csvParse from 'csv-parse';
 
 class ImportCategoryUseCase{
 
-    execute(file: any){
+    execute(file: Express.Multer.File): void{
+        const stream    = fs.createReadStream(file.path);
+        const parseFile = csvParse({
+            delimiter: ";"
+        });
+        stream.pipe(parseFile);
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
         console.log(file);
     }
 }
