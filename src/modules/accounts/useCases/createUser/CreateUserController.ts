ff2b91417914439db02ae215db8d3b5346222bd4
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-23 19:25:39 -0300

    Criado Controller User

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,7 +1,24 @@
+import {Request, Response} from "express";
+import {container} from "tsyringe";
+import {CreateUserUseCase} from "./CreateUserUseCase";
+
 class CreateUserController{
 
-    handle(){
-        
+    async handle(request: Request, response: Response): Promise<Response>{
+
+        const {name, username, password, email, driver_license} = request.body;
+
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        await createUserUseCase.execute({
+            name,
+            username,
+            password,
+            email,
+            driver_license
+        });
+
+        return response.status(201).send();
     }
 
 }
