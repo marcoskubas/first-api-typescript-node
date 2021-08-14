b720eccc261692f9a0f01313911d23f0ce184e8b
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-08-14 15:59:30 -0300

    Imports Application

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import "express-async-errors";
 import swaggerUI from 'swagger-ui-express';
 
 import "./database";
-import "./shared/container";
+import "@shared/container";
 import { router } from './routes';
 import swaggerFile from './swagger.json';
-import {AppError} from "./errors/AppError";
+import {AppError} from "@errors/AppError";
 
 const app = express();
 
